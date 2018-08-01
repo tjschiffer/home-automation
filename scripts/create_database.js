20730a144a3b7f5a6ee34fd99215e5c6b88a4c99
Unknown <tjschiffer@gmail.com>
2018-07-31 21:44:01 -0400

    fix script to use updated config

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -3,10 +3,10 @@ const dbconfig = require('../config/database');
 
 const connection = mysql.createConnection(dbconfig.connection);
 
-connection.query('DROP DATABASE ' + dbconfig.database);
-connection.query('CREATE DATABASE ' + dbconfig.database);
+connection.query('DROP DATABASE ' + dbconfig.connection.database);
+connection.query('CREATE DATABASE ' + dbconfig.connection.database);
 
-connection.query('CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
+connection.query('CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.users_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `username` VARCHAR(20) NOT NULL, \
     `password_hash` CHAR(95) NOT NULL, \
@@ -15,13 +15,13 @@ connection.query('CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_t
     UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
 )');
 
-connection.query('INSERT INTO `' + dbconfig.database + '`.`' + dbconfig.users_table + '` \
+connection.query('INSERT INTO `' + dbconfig.connection.database + '`.`' + dbconfig.users_table + '` \
   (`username`, `password_hash`) \
   VALUES \
   (\'api\', \'$argon2i$v=19$m=4096,t=3,p=1$gZv4zQqByDmPWwccHdanrA$IcN7WyV1TrkRPIeteKzK0cHNEUd6EDYC1MGEdwV+4lw\')');
 
 
-connection.query('CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.time_series_data_table + '` ( \
+connection.query('CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.time_series_data_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `sensor_id` SMALLINT UNSIGNED NOT NULL, \
     `timestamp` TIMESTAMP NOT NULL, \
@@ -30,6 +30,6 @@ connection.query('CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.time_se
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) \
 )');
 
-console.log('Success!');
+console.log('Finished');
 
 connection.end();
