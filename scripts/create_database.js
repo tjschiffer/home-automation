a0555c9a00df3c9b5880158f4122ee8b02fa7949
T.J. Schiffer <tjschiffer@gmail.com>
2018-08-21 00:27:52 +0000

    for loop for executing queries

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -1,115 +1,74 @@
 const mysql = require('mysql');
 const dbconfig = require('../config/database');
 
-async function createDatabase() {
-  // TODO: This would be cleaner as an reduce with an array of commands to be executed
+const createDatabase = async() => {
   const connectionConfig = Object.assign({}, dbconfig.connection);
   // The database cannot be defined, since this script creates that database
   delete connectionConfig['database'];
   const connection = mysql.createConnection(connectionConfig);
 
   const queries = [
-    'CREATE DATABASE IF NOT EXIST `' + dbconfig.connection.database + '`',
+    'CREATE DATABASE IF NOT EXISTS `' + dbconfig.connection.database + '`',
+
     'CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.users_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `username` VARCHAR(20) NOT NULL, \
     `password_hash` CHAR(95) NOT NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC))'
-  ];
-
-  // Drop the database if the argument "--drop" is present
-  'DROP DATABASE IF EXISTS `' + dbconfig.connection.database + '`'
-
-  let err;
-  err = await new Promise(resolve => {
-    connection.query('DROP DATABASE IF EXISTS `' + dbconfig.connection.database + '`', (err) => {
-      if (err) {
-        resolve(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-  if (err) {
-    return err;
-  }
-
-  err = await new Promise(resolve => {
-    connection.query('CREATE DATABASE `' + dbconfig.connection.database + '`', (err) => {
-      if (err) {
-        resolve(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-  if (err) {
-    return err;
-  }
-
-  err = await new Promise(resolve => {
-    connection.query('CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.users_table + '` ( \
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-    `username` VARCHAR(20) NOT NULL, \
-    `password_hash` CHAR(95) NOT NULL, \
-        PRIMARY KEY (`id`), \
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
     UNIQUE INDEX `username_UNIQUE` (`username` ASC))',
-      (err) => {
-        if (err) {
-          resolve(err);
-        } else {
-          resolve();
-        }
-      });
-  });
-  if (err) {
-    return err;
-  }
 
-  err = await new Promise(resolve => {
-    connection.query('INSERT INTO `' + dbconfig.connection.database + '`.`' + dbconfig.users_table + '` \
-    (`user`username`, `password_hash`) \
+    'INSERT INTO `' + dbconfig.connection.database + '`.`' + dbconfig.users_table + '` \
+    (`id`, `username`, `password_hash`) \
     VALUES \
-    (\'tj\', \'$argon2i$v=19$m=4096,t=3,p=1$sOr9QWi2qUsHG4nb0/13yQ$kRP8u3MHBRHzKtUmUIFXh01qZMVP+V1Nfdsts1xBWSM\'), \
-    (\'api\', \'$argon2i$v=19$m=4096,t=3,p=1$gZv4zQqByDmPWwccHdanrA$IcN7WyV1TrkRPIeteKzK0cHNEUd6EDYC1MGEdwV+4lw\')',
-      (err) => {
-        if (err) {
-          resolve(err);
-        } else {
-          resolve();
-        }
-      });
-  });
-  if (err) {
-    return err;
-  }
+    (1, \'tj\', \'$argon2i$v=19$m=4096,t=3,p=1$sOr9QWi2qUsHG4nb0/13yQ$kRP8u3MHBRHzKtUmUIFXh01qZMVP+V1Nfdsts1xBWSM\'), \
+    (2, \'api\', \'$argon2i$v=19$m=4096,t=3,p=1$gZv4zQqByDmPWwccHdanrA$IcN7WyV1TrkRPIeteKzK0cHNEUd6EDYC1MGEdwV+4lw\')',
 
-  err = await new Promise(resolve => {
-    connection.query('CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.time_series_data_table + '` ( \
+    'CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.time_series_data_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `sensor_id` SMALLINT UNSIGNED NOT NULL, \
     `timestamp` TIMESTAMP NOT NULL, \
     `value` FLOAT NOT NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC))',
-      (err) => {
+
+    'CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.sensors_table + '` ( \
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
+    `location` VARCHAR(30) NOT NULL, \
+    `measurement` VARCHAR(30) NOT NULL, \
+    `units` VARCHAR(20), \
+        PRIMARY KEY (`id`), \
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC))',
+
+    'INSERT INTO `' + dbconfig.connection.database + '`.`' + dbconfig.sensors_table + '` \
+    (`id`, `location`, `measurement`, `units`) \
+    VALUES \
+    (1, \'Living Room\', \'Temperature\', \'°C\'), \
+    (2, \'Living Room\', \'Humidity\', \'%\')'
+  ];
+
+  // Drop the database if the argument "--drop" is present
+  if (process.argv.slice(2).indexOf('--drop') > -1) {
+    queries.unshift('DROP DATABASE IF EXISTS `' + dbconfig.connection.database + '`' + '\n');
+  }
+
+  for (const query of queries) {
+    console.log('Executing statement: ' + query);
+    const executedQuery = await new Promise(resolve => {
+      connection.query(query, err => {
         if (err) {
           resolve(err);
         } else {
-          resolve();
+          resolve('Success!');
         }
       });
-  });
-  if (err) {
-    return err;
+    });
+    console.log('Result: ' + executedQuery+ '\n');
   }
 
   connection.end();
-  return 'Success!';
-}
+  return 'Complete.';
+};
 
 createDatabase().then((result) => {
   console.log(result);
